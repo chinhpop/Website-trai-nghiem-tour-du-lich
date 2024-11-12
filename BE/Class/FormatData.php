41d2b86ffa9733b788444804e69e04a0e2bd4fd1
Ngocphuoc <tranngocphuoc.2000vta@gmail.com>
2024-11-12 23:16:01 +0700

    Update

--- BE/Class/FormatData.php
+++ BE/Class/FormatData.php
@@ -5,24 +5,4 @@
         $formattedAmount = str_replace(',', '.', $formattedAmount); 
         return $formattedAmount . " VND";
     }
-
-    function formatData($data) {
-        $items = explode(', ', $data);
-        $formmatted = "<table border = '0' class='tour-table'>";
-        foreach ($items as $item) {
-            $formmatted.= "<tr><td>". $item. "</td></tr>";
-        }
-        $formmatted .= "</table>";
-        return $formmatted;
-    }
-
-    function formatPrice($price, $data) {
-        $items = explode(', ', $data);
-        $formmatted = "<table border = '0' class='tour-table'>";
-        foreach ($items as $item) {
-            $formmatted.= "<tr><td>". $price. "</td></tr>";
-        }
-        $formmatted .= "</table>";
-        return $formmatted;
-    }
 ?>
